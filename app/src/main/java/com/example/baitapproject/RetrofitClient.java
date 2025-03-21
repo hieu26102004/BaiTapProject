0e678a6ab097a64b6fb32ccffc89e85854ec96cd
VanLuanNguyen <vanluannguyen2004@gmail.com>
2025-03-21 08:36:10 +0700

    Tao RetrofitClient

---
+++ app/src/main/java/com/example/baitapproject/RetrofitClient.java
@@ -0,0 +1,17 @@
+package com.example.baitapproject;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    public static Retrofit getRetrofit(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
