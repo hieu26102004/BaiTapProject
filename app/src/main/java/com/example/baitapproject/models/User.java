9ceedfca17fd471b1b1af0a59118e0f7477ee006
VanLuanNguyen <vanluannguyen2004@gmail.com>
2025-03-21 08:29:44 +0700

    update model User

--- app/src/main/java/com/example/baitapproject/models/User.java
+++ app/src/main/java/com/example/baitapproject/models/User.java
@@ -1,4 +1,47 @@
 package com.example.baitapproject.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+    @SerializedName("userId")
+    private int userId;
+    @SerializedName("username")
+    private String username;
+    @SerializedName("password")
+    private String password;
+    @SerializedName("fullname")
+    private String fullname;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
